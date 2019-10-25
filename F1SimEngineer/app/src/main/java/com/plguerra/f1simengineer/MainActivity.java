120e633b481659bda0e2fc558da340eee36b4d27
Pablo Guerra <pguerra_16@me.com>
2019-10-24 22:26:54 -0500

    Initial Files

---
+++ F1SimEngineer/app/src/main/java/com/plguerra/f1simengineer/MainActivity.java
@@ -0,0 +1,56 @@
+package com.plguerra.f1simengineer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Obtain Buttons from activity_main.xml
+        Button Dashboard = findViewById(R.id.dashboard_button);
+        Button Analysis = findViewById(R.id.analysis_button);
+        Button Setting = findViewById(R.id.settings_button);
+
+        //Listening for Button Presses
+        Dashboard.setOnClickListener(this);
+        Analysis.setOnClickListener(this);
+        Setting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            //Handling conversion of button presses (digits) to string data.
+            case R.id.dashboard_button:
+                openSimpleDash();
+                break;
+
+            case R.id.analysis_button:
+                Toast.makeText(this,"Open Analysis Page", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.settings_button:
+                Toast.makeText(this,"Open Settings Page", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+    //Open Create Note Page
+    public void openSimpleDash(){
+        Intent intent = new Intent(this, SimpleDash.class);
+        startActivity(intent);
+    }
+}
+
---
+++ F1SimEngineer/app/src/main/java/com/plguerra/f1simengineer/SimpleDash.java
@@ -0,0 +1,19 @@
+package com.plguerra.f1simengineer;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleDash extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_simple);
+
+
+    }
+
+
+}
